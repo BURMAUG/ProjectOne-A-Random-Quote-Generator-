0defc636a0f12e078505e3a8e7a32789bfbf00bf
Deejai <burmaud1@gmail.com>
2020-05-27 16:25:43 -0400

    Functions Created

--- js/script.js
+++ js/script.js
@@ -14,24 +14,71 @@ project 1 - A Random Quote Generator
 let quote = [
   {quote:`Two things are infinite: the universe and human stupidity; and I'm not 
           sure about the universe.`,
-  source: 'Einsten'},
+  source: 'Albert Einsten'},
 
   {quote:`Two things are infinite: the universe and human stupidity; and I'm not sure
            about the universe.`,
-  source: 'Albert'},
+  source: 'Albert Einsten'},
 
   {quote:`I do not know what I may appear to the world, but to myself I seem to have
             been only like a boy playing on the sea-shore, and diverting myself in now 
             and then finding a smoother pebble or a prettier shell than ordinary, whilst the great
             ocean of truth lay all undiscovered before me.`,
-  source: 'Newton'},
+  source: 'Issac Newton'},
 
   {quote:`Don’t let the noise of others’ opinions drown out your own inner voice.`,
-  source: 'Steve'},
+  source: 'Steve Jobs',
+  year: 2005,
+  citation: 'Stanford University commencement speech'
+  },
 
   {quote:`Have the courage to follow your heart and intuition. They somehow already know what
           you truly want to become. Everything else is secondary.`,
-  source: 'Jobs'}
+  source: 'Steve Jobs'},
+  {
+    quote: `There are only two ways to live your life. One is as though nothing is 
+             a miracle. The other is as though everything is a miracle.`,
+    source: `Albert Einstein`
+  },
+  {
+    quote: `I am enough of an artist to draw freely upon my imagination. Imagination is more important than
+            knowledge. Knowledge is limited. Imagination encircles the world.`,
+    source: `Albert Einstein`,
+    year: 1929,
+    citation: `Veireck Interview`
+  },
+  {
+    quote:`If you can't explain it to a six year old, you don't understand it yourself`,
+    source:`Albert Einstein`
+  },
+  {
+    quote:`If you want your children to be intelligent, read them fairy tales. If you want 
+            them to be more intelligent, read them more fairy tales.`,
+    source:`Albert Einstein`
+  },
+  {
+    quote: `Taking LSD was a profound experience, one of the most important things in my life. 
+            LSD shows you that there’s﻿ another side to the coin, and you can’t remember it when 
+            it wears off, but you know it. It reinforced my sense of what was important—creating 
+            great things instead of making money, putting things back into the stream of history
+             and of human consciousness as much as I could.`,
+    source:'Steve Jobs',
+
+  },
+  {
+    quote: `Walter Issacson biographer of Steve Jobs: I remember sitting in his backyard in his 
+          garden, one day, and he started talking about God. He [Jobs] said, “ Sometimes I 
+          believe in God, sometimes I don’t. I think it’s 50/50, maybe. But ever since I’ve had 
+          cancer, I’ve been thinking about it more, and I find myself believing a bit more, 
+          maybe it’s because I want to believe in an afterlife, that when you die, it doesn’t 
+          just all disappear. The wisdom you’ve accumulated, somehow it lives on.”
+          Then he paused for a second and said, “Yea, but sometimes, I think it’s just
+          like an On-Off switch. Click. And you’re gone.” And then he paused again and 
+          said, “ And that’s why I don’t like putting On-Off switches on Apple devices.`,
+    source: 'Steve Jobs',
+    year: 2011,
+    citation: `Steve Jobs by Walter Issaacson`
+  }
             ];
 
 
